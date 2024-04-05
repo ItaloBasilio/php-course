46e2e7e97fa52e9be78506f3499f11aec1b6957f
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-05 01:32:49 -0300

    update

---
+++ Include/exemplo-02.php
@@ -0,0 +1,12 @@
+<?php
+
+// Include - Tenta funcionar mesmo com problemas e possui mais recursos
+//require - obriga que o arquivo funcione corretamente
+require_once "exemplo-01.php";
+require_once "exemplo-01.php";
+
+$resultado = somar(10,20);
+
+echo $resultado;
+
+?>
