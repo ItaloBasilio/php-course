90efcd3bfed6ffed1a85056c4a56f6b6028032ec
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-04 22:17:24 -0300

    Strings

---
+++ Strings/exemplo2.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = "joão rangel";
+$nome2 = "JOAO RANGEL";
+$nome3 = "joao rangel";
+$nome4 = "joao rangel";
+
+echo strtoupper($nome) ;
+
+echo "<br/>";
+
+echo strtolower($nome2) ;
+
+echo "<br/>";
+
+echo ucwords($nome3) ;
+
+echo "<br/>";
+
+echo ucfirst($nome4) ;
+
+
+?>
