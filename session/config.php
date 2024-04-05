22eb60902f4524efd73dd7c6c8d9cfcd539020ee
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-05 04:59:07 -0300

    update

--- session/config.php
+++ session/config.php
@@ -1,7 +1,16 @@
 <?php
 
-require_once('config.php');
+// Inicia a sessão
+session_start();
 
-$_SESSION['nome'] = "Hcode";
+// Configurações comuns
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'senha');
+// ...
+
+// Inclui outros arquivos necessários
+require_once('config.php');
+// ...
 
 ?>
