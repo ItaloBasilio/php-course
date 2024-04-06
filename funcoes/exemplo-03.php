cada49c33af1829bd21d1ae222ae57e476de18f6
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-05 23:47:05 -0300

    update

---
+++ funcoes/exemplo-03.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+function ola($texto, $periodo = "bom dia" ){
+
+    return "Ola $texto! $periodo! <br>";
+}
+
+echo ola("mundo");
+
+?>
