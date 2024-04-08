8336ebb0c9ecff0708f574af6ec6cce0a3e5d186
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-08 02:20:12 -0300

    update

---
+++ abstrated/exemplo-01.php
@@ -0,0 +1,49 @@
+<?php
+
+// Organizar o proprio codigo
+// Quando se trabalha com outros programadores
+// Quando o codigo ira comunicar com sistemas terceiros
+
+interface Veiculo {
+
+    public function acelerar($velocidade);
+    public function frenar($velocidade);
+    public function trocarMarcha($marcha);
+
+}
+
+
+abstract class Automovel implements Veiculo {
+
+    public function acelerar($velocidade)
+    {
+        echo "O veiculo acelerou até ". $velocidade . "Km/h";
+    }
+
+    public function frenar( $velocidade ) 
+    {
+        echo "O veiculo frenou até ". $velocidade . "Km/h";
+    }
+
+    public function trocarMarcha($marcha)
+    {
+        echo "O veiculo engatou a marcha ". $marcha;
+    }
+
+}
+
+class DelRey extends Automovel {
+
+    public function empurrar(){
+
+
+    }
+
+}
+
+$carro = new Automovel();
+
+$carro->acelerar(200);
+
+
+?>
