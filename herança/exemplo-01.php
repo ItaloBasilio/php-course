589e5427a048159f6bc50320ef4112123616dd4b
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-08 02:06:19 -0300

    update

---
+++ herança/exemplo-01.php
@@ -0,0 +1,41 @@
+<?php
+
+class Documento {
+    private $numero;
+
+    public function getNumero(){
+        return $this-> numero;
+    }
+
+    public function setNumero($numero){
+        $this->numero = $numero;
+    }
+}
+
+
+class CPF  extends Documento {
+ 
+    public function validar():bool
+    {
+
+        $numeroCPF =  $this-> getNumero();
+
+        //validação do CPF
+
+        return true;
+
+    }
+
+}
+
+$doc = new CPF();
+
+$doc->setNumero('24158746321');
+
+var_dump($doc->validar()) ;
+
+echo "<br/>";
+
+echo $doc->getNumero();
+
+?>
---
+++ interface/exemplo-01.php
@@ -0,0 +1,11 @@
+<?php
+
+// Organizar o proprio codigo
+// Quando se trabalha com outros programadores
+// Quando o codigo ira comunicar com sistemas terceiros
+
+
+
+
+
+?>
